995280d3337354025fde97fa859d24c69b8ba262
Gurpreet Singh <GurpreetSingh@malwapunjab.onmicrosoft.com>
2026-03-20 13:45:15 +0000

    Merged PR 13: ci: add package publishing to pipeline
    Related work items: #2

---
+++ az400_utils/__init__.py
@@ -0,0 +1,4 @@
+from .calculator import add, subtract, multiply, divide
+
+__version__ = '1.0.0'
+__all__ = ['add', 'subtract', 'multiply', 'divide']
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='az400-utils',
+    version='1.0.0',
+    packages=find_packages(),
+    description='AZ-400 lab utility functions',
+    author='Gurpreet Singh',
+)
